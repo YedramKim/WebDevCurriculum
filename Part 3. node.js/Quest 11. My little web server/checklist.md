fc13ceeb3f3cba4d93c1d0395236cafc79012212
YedarmKim <oncoder@naver.com>
2016-09-27 09:31:42 +0900

    insolve server.js

---
+++ Part 3. node.js/Quest 11. My little web server/checklist.md
@@ -0,0 +1,8 @@
+### HTTP의 GET과 POST 메소드는 어떻게 다른가요?
+* quest 01
+
+#### 다른 HTTP 메소드에는 무엇이 있나요?
+* quest 01 - 01
+
+### HTTP 서버에 GET과 POST를 통해 데이터를 보내려면 어떻게 해야 하나요?
+* quest 02
--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -1,5 +1,46 @@
-var http = require('http');
+var http = require("http");
+var urlParse = require("url");
+var qs = require('querystring');
 
 http.createServer(function(req, res) {
 	// TODO: 이 곳을 채워넣으세요..!
+	var url = req.url;
+	url = urlParse.parse(url, true);
+	var method = req.method; //GET or POST
+	var path = url.pathname;
+	var query = url.query;
+
+	if(path === "/foo") {
+		if(method === "GET") {
+			res.writeHead(200, {
+				"Content-Type" : "text/html"
+			});
+			res.end("Hello, " + query.bar);
+		}else if(method === "POST") {
+			var body = "";
+
+			req.on('data', function(data) {
+				body += data;
+			});
+
+			req.on('end', function(){
+				body = qs.parse(body);
+				res.writeHead(200, {
+					"Content-Type" : "text/html"
+				});
+				res.end("Hello, " + body.bar);
+			});
+		}
+	}else if(path === "/") {
+		res.writeHead(200, {
+			"Content-Type" : "text/html"
+		});
+		res.end("Hello World!");
+	}else{ // 그 외의 url은 리다이렉트
+		res.writeHead(302, {
+			"Location" : "/"
+		});
+		res.end("Redirecting");
+	}
+
 }).listen(8080);
