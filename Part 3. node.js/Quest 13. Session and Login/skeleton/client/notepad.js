735cd8996bcd6cd768c3321a62f1975371eff9a5
YedarmKim <oncoder@naver.com>
2016-09-30 16:17:07 +0900

    make loginpage and remove jquery script tag

--- Part 3. node.js/Quest 13. Session and Login/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 13. Session and Login/skeleton/client/notepad.js
@@ -1,3 +1,276 @@
-var Notepad = function() {
+//자신이 부모 노드에서 몇번째 인지 알아보는 함수
+HTMLElement.prototype.getIndex = function() {
+	var siblings = this.parentNode.children;
+	var length = siblings.length;
+	for(var i = 0; i < length; i++) {
+		if(siblings[i] == this) {
+			return i;
+		}
+	}
+}
+
+//메모장 객체
+var Notepad = function(selector) {
 	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
+	this.node = document.querySelector(selector); // 메모장 HTML 요소
+
+	// 메모장 리스트(HTML ul)
+	this.fileList = document.createElement("ul");
+
+	//메모 편집기 리스트(HTML ul)
+	this.fileEditorList = document.createElement("ul");
+
+	//메모 생성 버튼
+	this.lastlist = document.createElement("li");
+	this.createNoteButton = document.createElement("button");
+
+	this.files = null // 메모장 리스트(ajax로 불러오기, _initialize에서 데이터 초기화)
+
+	this._initialize();
 };
+Notepad.prototype._initialize = function() {
+	var notepad = this; // Notepad 객체 자기자신
+
+	// 메모장 리스트(HTML ul)
+	this.fileList.classList.add("filetab");
+	this.fileList.classList.add("list");
+	this.node.appendChild(this.fileList);
+
+	//메모 편집기 리스트(HTML ul)
+	this.fileEditorList.classList.add("noteEditors");
+	this.node.appendChild(this.fileEditorList);
+
+	//메모 생성 버튼
+	this.lastlist.classList.add("createNote");
+	this.createNoteButton.innerHTML = "<span class='octicons octicon-plus'></span>파일 생성";
+	this.lastlist.appendChild(this.createNoteButton);
+
+	//메모장 리스트 가져오기
+	fetch("/note").then(function(res) {
+		if(res.ok) {
+			return res.json();
+		}
+	}).then(function(data) {
+		notepad.files = data;
+		notepad.start();
+		notepad.bindEvent();
+	});
+}
+//메모장 실행 함수
+Notepad.prototype.start = function() {
+	var notepad = this;
+	//파일 버튼 삽입에 쓸 이벤트 함수
+	function buttonAppendEvent(e) {
+		notepad.fileList.appendChild(this); // 파일목록에 파일 부르기 버튼 삽입
+	}
+	//파일 편집기 삽입에 쓸 이벤트 함수
+	function editorAppendEvent(e) {
+		notepad.fileEditorList.appendChild(this);
+	}
+
+	//메모장 목록 생성
+	var length = this.files.length;
+	for(var i = 0; i < length; i++) {
+		var note = new Note(this.files[i]);
+		note.loadDom.addEventListener("append", buttonAppendEvent);
+		note.noteEditor.addEventListener("append", editorAppendEvent);
+		note._initialize();
+		this.files[i] = note;
+	}
+	notepad.fileList.appendChild(notepad.lastlist);
+}
+//메모장 관련 이벤트 설정
+Notepad.prototype.bindEvent = function() {
+	//새 파일 생성
+	var notepad = this;
+
+	notepad.createNoteButton.addEventListener("click", createNote);
+	function createNote(e) {
+		//제목 입력
+		var title = prompt("생성할 파일의 이름을 입력하세요.\n(참고로 생성한 파일은 저장하지 않고 사이트를 나가면 사라집니다.)");
+		if(title === null) {
+			return ;
+		}
+
+		title = title.trim();
+		if(title === "") {
+			alert("제목을 입력하세요.")
+			return;
+		}
+
+		//파일 버튼 삽입에 쓸 이벤트 함수
+		function buttonAppendEvent(e) {
+			notepad.fileList.appendChild(this); // 파일목록에 파일 부르기 버튼 삽입
+		}
+		//파일 편집기 삽입에 쓸 이벤트 함수
+		function editorAppendEvent(e) {
+			notepad.fileEditorList.appendChild(this);
+		}
+
+		//파일 생성
+		var data = {
+			title : title,
+			content : ""
+		};
+		var note = new Note(data);
+
+		note.loadDom.addEventListener("append", buttonAppendEvent);
+		note.noteEditor.addEventListener("append", editorAppendEvent);
+		note._initialize();
+		note.showEditor();
+		notepad.files.push(note);
+
+		//파일 생성 버튼을 다시 맨뒤에 놓음
+		notepad.fileList.appendChild(notepad.lastlist);
+	}
+
+	//탭으로 메모 전환
+	document.body.addEventListener("keydown", function(e) {
+		//누른 키가 탭일 경우
+		if(e.which == 9 && e.key.toLowerCase() == "tab") {
+			e.preventDefault();
+			var noteNum = notepad.files.length;
+			
+			//메모 파일이 없을 경우 종료
+			if(noteNum === 0) {
+				return ;
+			}
+
+			var current = notepad.fileList.querySelector(".select");
+			var clickEventTrigger = new Event("click");
+			if(current !== null) {
+				//쉬프트 키가 눌려져 있을 경우
+				if(this.shiftPress) {
+					var idx = current.getIndex() - 1;
+					if(idx < 0) {
+						idx = noteNum - 1;
+					}
+				}else { // 눌려져 있지 않은 경우
+					var idx = current.getIndex() + 1;
+					if(idx === noteNum) {
+						idx = 0;
+					}
+				}
+
+				notepad.files[idx].loadButton.dispatchEvent(clickEventTrigger);
+			}else {
+				//쉬프트 키가 눌려져 있을 경우 맨 마지막을 연다.
+				if(this.shiftPress) {
+					notepad.files[noteNum - 1].loadButton.dispatchEvent(clickEventTrigger);
+				}else{
+					notepad.files[0].loadButton.dispatchEvent(clickEventTrigger);
+				}
+			}
+		}
+
+		//누른 키가 쉬프트일 경우
+		if(e.which == 16 && e.key.toLowerCase() == "shift") {
+			this.shiftPress = true;
+		}
+	});
+
+	document.body.addEventListener("keyup", function(e) {
+		//쉬프트키를 땠을 경우
+		if(e.which == 16 && e.key.toLowerCase() == "shift") {
+			this.shiftPress = false;
+		}
+	});
+}
+
+//메모장 파일(처음에는 제목만 받고 클릭이벤트를 받으면 내용을 로드한다.
+var Note = function(data) {
+	this.data = data; //메모 데이터
+
+	// 더블클릭하면 내용을 로드하는 버튼
+	this.loadDom = document.createElement("li");
+	this.loadButton = document.createElement("button");
+
+	//메모장 편집기
+	this.noteEditor = document.createElement("li");
+	this.noteTextarea = document.createElement("textarea");
+
+	this.saveButton = document.createElement("button");
+}
+Note.prototype._initialize = function() {
+	this.loadButton.setAttribute("title", this.data.title);
+	this.loadDom.appendChild(this.loadButton);
+	this.loadButton.innerHTML ='<span class="octicons octicon-bookmark"></span>' + this.data.title + '&nbsp;(open)';	
+
+	this.noteEditor.appendChild(this.noteTextarea);
+
+	this.saveButton.innerHTML = '<span class="octicons octicon-keyboard"></span>저장하기';
+	this.noteEditor.appendChild(this.saveButton);
+
+	//메모 파일 추가 이벤트 발생
+	var eventTrigger = new Event("append");
+	this.loadDom.dispatchEvent(eventTrigger);
+	this.noteEditor.dispatchEvent(eventTrigger);
+
+	//이벤트 설정
+	this.bindEvent();
+}
+//이벤트 설정
+Note.prototype.bindEvent = function() {
+	var note = this;
+
+	//더블클릭 했을 시 데이터가 없으면 파일을 로드한다.
+	this.loadButton.addEventListener("click", loadNoteEvent);
+	function loadNoteEvent(e) {
+		e.preventDefault();
+		if(note.data.content === undefined) {
+			fetch("/note/load/" + note.data.idx).then(function(res) {
+				if(res.ok) {
+					return res.text();
+				}else{
+					alert("데이터를 불러오지 못햇습니다.");
+				}
+			}).then(function(data) {
+				note.data.content = data;
+				note.noteTextarea.textContent = data;
+				note.showEditor();
+			});
+		}else{
+			note.showEditor();
+		}
+	}
+
+	//메모 저장 및 수정
+	this.saveButton.addEventListener("click", noteSave);
+	function noteSave() {
+		note.data.content = note.noteTextarea.value;
+
+		var data = JSON.stringify(note.data);
+		fetch("/note/save", {
+			headers : {
+				"Content-Type" : "application/json"
+			},
+			method : "POST",
+			body : data
+		}).then(function(req) {
+			if(note.data.idx === undefined && req.ok) {
+				return req.text();
+			}else{
+				return note.data.idx;
+			}
+		}).then(function(idx) {
+			idx = parseInt(idx);
+			note.data.idx = idx;
+		});
+	}
+}
+//편집기 보여주기
+Note.prototype.showEditor = function() {
+	var siblings = this.loadDom.parentNode.children;
+	var length = siblings.length;
+	for(var i = 0; i < length; i++) {
+		siblings[i].classList.remove("select");
+	}
+	this.loadDom.classList.add("select");
+
+	var siblings = this.noteEditor.parentNode.children;
+	var length = siblings.length;
+	for(var i = 0; i < length; i++) {
+		siblings[i].classList.remove("edit");
+	}
+	this.noteEditor.classList.add("edit");
+}
--- Part 3. node.js/Quest 13. Session and Login/skeleton/server.js
+++ Part 3. node.js/Quest 13. Session and Login/skeleton/server.js
@@ -1,14 +1,164 @@
 var express = require('express'),
 	path = require('path'),
+	fs = require('fs'),
+	bodyParser = require('body-parser'),
+	session = require('express-session'),
 	app = express();
 
-app.use(express.static('client'));
-
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static(path.join(__dirname, "client")));
 
 /* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
+
+app.use(bodyParser.urlencoded({ extended : true})); // x-www-form-urlencoded 파싱
+app.use(bodyParser.json()); //JSON 파싱
+app.use(session({
+	secret : "secretNotepad",
+	resave : false
+})); // 서버에 세션 적용
+
+// /foo 관련 라우트
+var fooRouter = app.route("/foo");
+fooRouter.get(function (req, res) {
+	var query = req.query;
+	console.log("Hello, " + query.bar);
+	res.json(query);
+});
+fooRouter.post(function (req, res) {
+	var body = req.body;
+	console.log("Hello, " + body.bar);
+	res.send("");
+});
+
+//css 압축 관련 라우트
+var styleRouter = express.Router();
+styleRouter.use(function(req, res, next) {
+	res.root = path.join(__dirname, "client");
+	res.css = function (cssStr) {
+		res.set("Content-Type", "text/css");
+		res.send(cssStr.toString().split(/\/\*.+\*\/|\t|\r\n|\n/).join(""));
+	}
+	next();
+});
+styleRouter.get("/index", function(req, res) {
+	fs.readFile(path.join(res.root, "style.css"), function(error, style) {
+		style = style.toString();
+		res.css(style);
+	});
+});
+styleRouter.get("/octicons.ttf", function(req, res) {
+	//res.set("Content-Type", "application/x-font-opentype");
+	fs.readFile(path.join(res.root, "octicons.ttf"), function(error, font) {
+		font = font.toString();
+		res.send(font);
+	});
+});
+app.use('/style', styleRouter);
+
+//회원 목록
+var users = [
+	{
+		id : "admin",
+		password : "1234",
+		nickname : "집게사장"
+	},
+	{
+		id : "employee1",
+		password : "1234",
+		nickname : "징징이"
+	},
+	{
+		id : "employee2",
+		password : "1234",
+		nickname : "스폰지밥"
+	}
+];
+//회원 관련 세션 처리
+app.use(function(req, res, next) {
+	if(req.session.pageId !== undefined && typeof req.session.pageId === "string"){
+		req.login = true;
+	}else{
+		req.login = false;
+	}
+	next();
+});
+var loginRouter = express.Router();
+loginRouter.get("/page", function(req, res) {
+	res.sendFile(path.join(__dirname, "client", "login.html"));
+})
+app.use("/login", loginRouter);
+
+//메모장 관련 데이터
+var notes = [
+];
+var noteAutoIncrement = notes.length; // 새 파일을 만들 때 마다 사용할 메모 idx
+
+//메모장 관련 라우트
+var noteRouter = express.Router();
+//메모장 리스트를 전송
+noteRouter.get('/', function(req, res) {
+	//제목들만 전송
+	var datas = [];
+	var length = notes.length;
+	for(var i = 0; i < length; i++) {
+		datas.push({
+			idx : notes[i].idx,
+			title : notes[i].title,
+			id : undefined
+		});
+	}
+	res.json(datas);
+});
+//메모정보 전부 공개
+noteRouter.get('/all', function(req, res) {
+	res.json(notes);
+});
+//지정한 메모 로드
+noteRouter.get('/load/:idx' , function(req, res) {
+	var idx = parseInt(req.params.idx);
+	var data = notes.filter(function(note) {
+		return note.idx === idx;
+	})[0];
+	if(data) {
+		res.send(data.content);
+	}else{
+		res.send("");
+	}
+});
+//메모 저장
+noteRouter.post("/save", function(req, res) {
+	var data = req.body;
+	//기존에 있던 파일일 경우
+	if(data.idx !== undefined) {
+		var length = notes.length;
+		for(var i = 0; i < length; i++) {
+			if(data.idx === notes[i].idx) {
+				notes[i].content = data.content;
+			}
+		}
+		res.send("save");
+	}else { // 기존에 있던 파일이 아닐 경우 (메모장 목록에 추가, 데이터에 idx추가)
+		data.idx = noteAutoIncrement++;
+		notes.push(data);
+		res.send(data.idx+"");
+	}
+});
+app.use('/note', noteRouter);
+
+app.get('/', function (req, res) {
+	//로그인이 되어있지 않을 경우
+	if(req.login === false) {
+		console.log("not login");
+		res.set("Content-Type", "text/html");
+		res.write("<meta charset='utf-8'><script>alert('로그인을 하세요.'); location = '/login/page';</script>");
+	}else{
+		res.sendFile(path.join(__dirname, 'client', 'notepad.html'));
+	}
+	res.end();
+});
+
+app.get('/*', function(req, res) {
+	res.redirect(302, "/");
+});
 
 var server = app.listen(8080, function () {
 	console.log('Server started!');
