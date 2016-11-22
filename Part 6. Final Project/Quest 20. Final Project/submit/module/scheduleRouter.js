833eab65539f6ce23b74fef210fbe8460a6e4c47
YedarmKim <oncoder@naver.com>
2016-11-22 16:50:40 +0900

    add paper-checkbox and iron-form

--- Part 6. Final Project/Quest 20. Final Project/submit/module/scheduleRouter.js
+++ Part 6. Final Project/Quest 20. Final Project/submit/module/scheduleRouter.js
@@ -4,3 +4,7 @@ var router = module.exports = exports = new express.Router();
 
 //데이터베이스 관련 모듈 객체
 const Database = require("./sequelize");
+
+router.post("/schedule/register", (req, res) => {
+	res.send(req.body);
+});
